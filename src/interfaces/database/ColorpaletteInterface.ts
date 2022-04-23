c2961979d834ec10fe20d37fcb27c5eb1f44add1
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-04-23 12:48:37 +0200

    This project is dead

--- src/interfaces/database/ColorpaletteInterface.ts
+++ src/interfaces/database/ColorpaletteInterface.ts
@@ -1,12 +1,8 @@
+interface ColorPaletteInterface {
+  name: string;
+  colors: string[];
+  id: string;
+  colorTemperature: string;
+}
 
-
-
-
-
-
-
-
-
-
-
-
+export default ColorPaletteInterface;
--- src/server/schemas.ts
+++ src/server/schemas.ts
@@ -3,12 +3,20 @@ import { gql } from "apollo-server-express";
 const typeDefs = gql`
   type Query {
     hello: String
-    getColorPalettes: [ColorPalette!]
+    getColorPalettes(input: ColorPaletteInput): [ColorPalette]!
+    getColorPalette(input: ColorPaletteInput): ColorPalette
   }
+
   type ColorPalette {
     id: ID!
     name: String!
     colors: [String!]
+    colorTemperature: String
+  }
+
+  input ColorPaletteInput {
+    colorTemperature: String
+    id: String
   }
 `;
 
